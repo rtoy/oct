4724a1da2deda8fe361cbc64e9c276e7fdaa492c
toy <toy>
2007-04-10 01:37:27 +0000

    o Move sqrt-qd function here, from qd.lisp. o Add functions needed for computing some trig functions:   - SINCOS-QD   - ATAN2-QD  (This seems not to work.) o Add some functions to experiment with Cordic algorithms for   double-floats.

--- qd-fun.lisp
+++ qd-fun.lisp
@@ -1,3 +1,26 @@
+(defun sqrt-qd (a)
+  (declare (type %quad-double a)
+	   (optimize (speed 3) (space 0)))
+  ;; Perform the following Newton iteration:
+  ;;
+  ;;  x' = x + (1 - a * x^2) * x / 2
+  ;;
+  ;; which converges to 1/sqrt(a).
+  (when (= a 0)
+    (return-from sqrt-qd #c(0w0 0w0)))
+
+  (let* ((r (make-qd-d (/ (sqrt (the (double-float (0d0))
+				  (qd-0 a)))) 0d0 0d0 0d0))
+	 (half (make-qd-dd 0.5w0 0w0))
+	 (h (mul-qd a half)))
+    (declare (type %quad-double r))
+    ;;(setf h (mul-qd-d a .5d0))
+    (setf r (add-qd r (mul-qd r (sub-qd half (mul-qd h (sqr-qd r))))))
+    (setf r (add-qd r (mul-qd r (sub-qd half (mul-qd h (sqr-qd r))))))
+    (setf r (add-qd r (mul-qd r (sub-qd half (mul-qd h (sqr-qd r))))))
+    (mul-qd r a)))
+
+
 (defun nint-qd (a)
   (let ((x0 (fround (qd-0 a)))
 	(x1 0d0)
@@ -132,13 +155,13 @@
 	   (p a)
 	   (m 1d0))
       (loop
-	  (setf p (mul-qd p x))
-	  (incf m 2)
-	(setf p (div-qd-d p (* m (1- m))))
+	 (setf p (mul-qd p x))
+	 (incf m 2)
+	 (setf p (div-qd-d p (* m (1- m))))
 	 (setf s (add-qd s p))
-	 (format t "p = ~A~%" (qd-0 p))
-	(when (< (abs (qd-0 p)) thresh)
-	  (return)))
+	 ;;(format t "p = ~A~%" (qd-0 p))
+	 (when (< (abs (qd-0 p)) thresh)
+	   (return)))
       ;; cos(c) = sqrt(1-sin(c)^2).  This seems to work ok, even
       ;; though I would have expected some round-off errors in
       ;; computing this.  sqrt(1-x^2) is normally better computed as
@@ -320,3 +343,217 @@
 		       ;; cos(j*pi/2) = -1, sin(j*pi/2) = 0
 		       (neg-qd c)))))))))))
 
+;; Compute sin and cos of a
+(defun sincos-qd (a)
+  (declare (type %quad-double a))
+  (when (= a 0)
+    (return-from sincos-qd
+      (values (make-qd-dd 0w0 0w0)
+	      (make-qd-dd 1w0 0w0))))
+
+  ;; Reduce modulo 2*pi
+  (let ((r (drem-qd a +qd-2pi+)))
+    ;; Now reduce by pi/2 and then by pi/1024 so that we obtain
+    ;; numbers a, b, t
+    (multiple-value-bind (j tmp)
+	(divrem-qd r +qd-pi/2+)
+      (let* ((j (truncate (qd-0 j)))
+	     (abs-j (abs j)))
+	(multiple-value-bind (k tmp)
+	    (divrem-qd tmp +qd-pi/1024+)
+	  (let* ((k (truncate (qd-0 k)))
+		 (abs-k (abs k)))
+	    (assert (<= abs-j 2))
+	    (assert (<= abs-k 256))
+	    ;; Compute sin(s) and cos(s)
+	    (multiple-value-bind (sin-t cos-t)
+		(sincos-taylor tmp)
+	      (multiple-value-bind (s c)
+		  (cond ((zerop abs-k)
+			 (values sin-t cos-t))
+			(t
+			 ;; Compute sin(s+k*pi/1024), cos(s+k*pi/1024)
+			 (let ((u (aref +qd-cos-table+ (1- abs-k)))
+			       (v (aref +qd-sin-table+ (1- abs-k))))
+			   (cond ((plusp k)
+				  ;; sin(s) * cos(k*pi/1024)
+				  ;; + cos(s) * sin(k*pi/1024)
+				  ;;
+				  ;; cos(s) * cos(k*pi/1024)
+				  ;; - sin(s) * sin(k*pi/1024)
+				  (values (add-qd (mul-qd u sin-t)
+						  (mul-qd v cos-t))
+					  (sub-qd (mul-qd u cos-t)
+						  (mul-qd v sin-t))))
+				 (t
+				  ;; sin(s) * cos(k*pi/1024)
+				  ;; - cos(s) * sin(|k|*pi/1024)
+				  ;;
+				  ;; cos(s) * cos(k*pi/1024)
+				  ;; + sin(s) * sin(|k|*pi/1024)
+				  (values (sub-qd (mul-qd u sin-t)
+						  (mul-qd v cos-t))
+					  (add-qd (mul-qd u cos-t)
+						  (mul-qd v sin-t))))))))
+		(format t "s = ~/qd-format/~%" s)
+		(format t "c = ~/qd-format/~%" c)
+		;; sin(x) =  sin(s+k*pi/1024) * cos(j*pi/2)
+		;;         + cos(s+k*pi/1024) * sin(j*pi/2)
+		(cond ((zerop abs-j)
+		       ;; cos(j*pi/2) = 1, sin(j*pi/2) = 0
+		       (values s c))
+		      ((= j 1)
+		       ;; cos(j*pi/2) = 0, sin(j*pi/2) = 1
+		       (values c (neg-qd s)))
+		      ((= j -1)
+		       ;; cos(j*pi/2) = 0, sin(j*pi/2) = -1
+		       (values (neg-qd c) s))
+		      (t
+		       ;; cos(j*pi/2) = -1, sin(j*pi/2) = 0
+		       (values (neg-qd s)
+			       (neg-qd c))))))))))))
+
+  
+(defun atan2-qd (y x)
+  (declare (type %quad-double y x))
+  ;; Instead of using Taylor series to compute atan, we instead use
+  ;; Newton's iteration to solve the equation
+  ;;
+  ;;   sin(z) = y/r or cos(z) = x/r
+  ;;
+  ;; where r = sqrt(x^2+y^2)
+  ;;
+  ;; The iteration is
+  ;;
+  ;;   z' = z + (y - sin(z))/cos(z)       (for sin)
+  ;;   z' = z + (x - cos(z))/sin(z)       (for cos)
+  ;;
+  ;; Here, x and y are normalized so that x^2 + y^2 = 1.
+  ;;
+  ;; If |x| > |y|, then the first iteration is used since the
+  ;; denominator is larger.  Otherwise the second is used.
+  (cond ((= x 0)
+	 (cond ((= y 0)
+		(error "atan2(0,0)"))
+	       (t
+		(return-from atan2-qd
+		  (cond ((qd-> y (make-qd-dd 0w0 0w0))
+			 +qd-pi/2+)
+			(t
+			 (neg-qd +qd-pi/2+)))))))
+	((= y 0)
+	 (return-from atan2-qd
+	   (cond ((qd-> x (make-qd-dd 0w0 0w0))
+		  (make-qd-dd 0w0 0w0))
+		 (t
+		  +qd-pi+)))))
+
+  (when (= x y)
+    (return-from atan2-qd
+      (if (qd-> y (make-qd-dd 0w0 0w0))
+	  +qd-pi/4+
+	  +qd-3pi/4+)))
+
+  (when (= x (neg-qd y))
+    (return-from atan2-qd
+      (if (qd-> y (make-qd-dd 0w0 0w0))
+	  +qd-3pi/4+
+	  (neg-qd +qd-pi/4+))))
+
+  (let* ((r (sqrt-qd (add-qd (sqr-qd x)
+			     (sqr-qd y))))
+	 (xx (div-qd x r))
+	 (yy (div-qd y r)))
+    (format t "r  = ~/qd-format/~%" r)
+    (format t "xx = ~/qd-format/~%" xx)
+    (format t "yy = ~/qd-format/~%" yy)
+    
+    ;; Compute double-precision approximation to atan
+    (let ((z (%make-qd-d (atan (qd-0 y) (qd-0 x)) 0d0 0d0 0d0))
+	  (sinz (make-qd-dd 0w0 0w0))
+	  (cosz (make-qd-dd 0w0 0w0)))
+      (cond ((qd-> xx yy)
+	     ;; Newton iteration  z' = z + (y - sin(z))/cos(z)
+	     (dotimes (k 3)
+	       (multiple-value-setq (sinz cosz) (sincos-qd z))
+	       (setf z (add-qd z (div-qd (sub-qd yy sinz)
+					 cosz)))))
+	    (t
+	     ;; Newton iteration z' = z - (x - cos(z))/sin(z)
+	     (dotimes (k 3)
+	       (multiple-value-setq (sinz cosz) (sincos-qd z))
+	       (format t "sinz ~/qd-format/~%" sinz)
+	       (format t "cosz ~/qd-format/~%" cosz)
+	       (setf z (sub-qd z (div-qd (sub-qd yy cosz)
+					 sinz))))))
+      z)))
+
+(defun atan-d (y x)
+  (let* ((r (abs (complex x y)))
+	 (xx (/ x r))
+	 (yy (/ y r)))
+    (let ((z (atan (float y 1f0) (float x 1f0)))
+	  (sinz 0d0)
+	  (cosz 0d0))
+      (format t "z = ~A~%" z)
+      (cond ((> xx yy)
+	     (dotimes (k 3)
+	       (let ((sinz (sin z))
+		     (cosz (cos z)))
+		 (setf z (+ z (/ (- y sinz)
+				 cosz))))))
+	    (t
+	     (dotimes (k 20)
+	       (let ((sinz (sin z))
+		     (cosz (cos z)))
+		 (format t "sz, dz = ~A ~A~%" sinz cosz)
+		 
+		 (setf z (- z (/ (- x cosz)
+				 sinz)))
+		 (format t "z = ~A~%" z)))))
+      z)))
+
+(defvar *table*)
+(defvar *ttable*)
+
+(defun setup-cordic ()
+  (let ((table (make-array 34))
+	(ttable (make-array 34)))
+    (setf (aref table 0) 1d0)
+    (setf (aref table 1) 1d0)
+    (setf (aref table 2) 1d0)
+    (setf (aref ttable 0) (/ pi 4))
+    (setf (aref ttable 1) (/ pi 4))
+    (setf (aref ttable 2) (/ pi 4))
+    (loop for k from 3 below 34 do
+	 (setf (aref table k) (* 0.5d0 (aref table (1- k))))
+	 (setf (aref ttable k) (atan (aref table k))))
+    (setf *table* table)
+    (setf *ttable* ttable)))
+
+
+(defun cordic-rot (x y)
+  (let ((z 0))
+    (dotimes (k (length *table*))
+      (cond ((minusp y)
+	     (psetq x (- x (* y (aref *table* k)))
+		    y (+ y (* x (aref *table* k))))
+	     (decf z (aref *ttable* k)))
+	    (t
+	     (psetq x (+ x (* y (aref *table* k)))
+		    y (- y (* x (aref *table* k))))
+	     (incf z (aref *ttable* k)))))
+    (values z x y)))
+
+(defun atan2-d (y x)
+  (multiple-value-bind (z dx dy)
+      (cordic-rot x y)
+    (let ((theta (/ dy dx)))
+      (format t "theta = ~A~%" theta)
+      (let ((corr (+ theta
+		     (- (/ (expt theta 3)
+			   3))
+		     (/ (expt theta 5)
+			5))))
+	(format t "corr = ~A~%" corr)
+	(+ z corr)))))
